3c933575d54d4403dcc315f79c3a9a578f960b1e
Marcelo.Guerra <penamarcelo3@gmail.com>
2026-02-09 08:33:43 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -2093,7 +2093,7 @@ with col_principal:
             
             with col_p2:
                 setor = st.selectbox("Setor:",
-                                    options=["Presidência","Desembargadora(a)","Gabinete", "Plenário", "Geral", "Cartório","Setores Administrativos"],
+                                    options=["Presidência","Desembargador(a)","Gabinete", "Plenário", "Geral", "Cartório","Setores Administrativos"],
                                     key="toolbar_setor")
             
             # Direcionar para colaborador específico
